c7fd177eb2752c68986c4f230ec9b734dd36364d
Ace.c <>
2019-10-17 13:40:36 +0800

    fix marge

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/videoList">Video</router-link>|
-      <router-link to="/loginAndRegistered">LoginAndRegistered</router-link>
+      <router-link to="/loginAndRegistered">LoginAndRegistered</router-link>|
       <router-link to="/about">About</router-link>|
       <router-link to="/newaddress">NewAddress</router-link>|
       <router-link to="/profile">profile</router-link>|
