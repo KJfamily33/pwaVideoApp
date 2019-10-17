ee9acd7698b50c493b7ef749c6d29bf3de2cc76e
Ace.c <>
2019-10-17 11:23:30 +0800

    done with js

--- src/views/Share.vue
+++ src/views/Share.vue
@@ -15,27 +15,79 @@
       <!--按鈕-->
       <div class="row margin-top-43">
         <div class="column">
-          <button class="text text-color-ffffff text-20-500">分享推广连结</button>
+          <button class="text text-color-ffffff text-20-500" @click="shareAlert(1)">分享推广连结</button>
         </div>
         <div class="column">
-          <button class="text text-color-ffffff text-20-500 alpha">复制推广码</button>
+          <button class="text text-color-ffffff text-20-500 alpha" @click="shareAlert(2)">复制推广码</button>
         </div>
       </div>
       <!--底部-->
       <div class="text text-color-ffffff text-17-500 margin-top-43">分享且成功注册,免费领取V币</div>
       <!--提視窗背景-->
-      <div class="coverBG"></div>
+      <div id="coverbg" class="coverBG"></div>
       <!--提視窗分享-->
-      <div class="share">
+      <div id="share" class="share">
         <div class="text text-color-f3806f text-20-500 margin-top-bottom-10">我要分享</div>
         <div class="line margin-left-right-6 margin-bottom-30"></div>
         <div class="share-row">
-          <div class="share-item margin-left-right-25 margin-bottom-20">123</div>
-          <div class="share-item margin-left-right-25 margin-bottom-20">466</div>
-          <div class="share-item margin-left-right-25 margin-bottom-20">123</div>
-          <div class="share-item margin-left-right-25 margin-bottom-20">123</div>
-          <div class="share-item margin-left-right-25 margin-bottom-20">466</div>
-          <div class="share-item margin-left-right-25 margin-bottom-20">123</div>
+          <!--QQ-->
+          <div class="share-item margin-left-right-25 margin-bottom-20" @click="shareSelected(1)">
+            <div class="share-app">
+              <svg-icon name="ic-qq" width="66" height="66"></svg-icon>
+            </div>
+            <div class="text text-color-6a6a6a text-16-300">QQ</div>
+          </div>
+          <!--微信-->
+          <div class="share-item margin-left-right-25 margin-bottom-20" @click="shareSelected(2)">
+            <div class="share-app">
+              <svg-icon name="ic-wechat" width="66" height="66"></svg-icon>
+            </div>
+            <div class="text text-color-6a6a6a text-16-300">微信</div>
+          </div>
+          <!--腾讯微博-->
+          <div class="share-item margin-left-right-25 margin-bottom-20" @click="shareSelected(3)">
+            <div class="share-app">
+              <svg-icon name="ic-weibo" width="66" height="66"></svg-icon>
+            </div>
+            <div class="text text-color-6a6a6a text-16-300">腾讯微博</div>
+          </div>
+          <!--新浪微博-->
+          <div class="share-item margin-left-right-25 margin-bottom-20" @click="shareSelected(4)">
+            <div class="share-app">
+              <svg-icon name="ic-sina" width="66" height="66"></svg-icon>
+            </div>
+            <div class="text text-color-6a6a6a text-16-300">新浪微博</div>
+          </div>
+          <!--网易-->
+          <div class="share-item margin-left-right-25 margin-bottom-20" @click="shareSelected(5)">
+            <div class="share-app">
+              <svg-icon name="ic-nets" width="66" height="66"></svg-icon>
+            </div>
+            <div class="text text-color-6a6a6a text-16-300">网易</div>
+          </div>
+          <!--飞秋-->
+          <div class="share-item margin-left-right-25 margin-bottom-20" @click="shareSelected(6)">
+            <div class="share-app">
+              <svg-icon name="ic-feiq" width="66" height="66"></svg-icon>
+            </div>
+            <div class="text text-color-6a6a6a text-16-300">飞秋</div>
+          </div>
+          <!--取消-->
+          <div
+            id="shareSeleted"
+            class="text text-color-f3806f text-20-300 padding-top-bottom-15 cancel-bg"
+            @click="shareSelected(0)"
+          >取消</div>
+        </div>
+      </div>
+      <!--提示窗已複製-->
+      <div id="copy" class="copy">
+        <div class="height-118 flex-center">
+          <div class="text text-25-500 text-color-f3806f">已复制链接</div>
+        </div>
+        <div class="line"></div>
+        <div class="height-50 flex-center" @click="shareSelected(0)">
+          <div class="text text-20-500 text-color-f3806f">确定</div>
         </div>
       </div>
     </div>
@@ -48,7 +100,32 @@ import { Component, Vue } from "vue-property-decorator";
 @Component({
   components: {}
 })
-export default class Share extends Vue {}
+export default class Share extends Vue {
+  // 分享選項
+  shareSelected(index: number) {
+    // 0: 取消 1: QQ 2: 微信 3: 騰訊微博 4: 新浪微博 5: 網易 6: 非秋
+    let coverbg = document.getElementById("coverbg") as HTMLDivElement;
+    let share = document.getElementById("share") as HTMLDivElement;
+    let copy = document.getElementById("copy") as HTMLDivElement;
+    coverbg.style.display = "none";
+    share.style.display = "none";
+    copy.style.display = "none";
+  }
+
+  // 分享連結
+  shareAlert(index: number) {
+    // 1: share 2: copy
+    let coverbg = document.getElementById("coverbg") as HTMLDivElement;
+    let share = document.getElementById("share") as HTMLDivElement;
+    let copy = document.getElementById("copy") as HTMLDivElement;
+    coverbg.style.display = "block";
+    if (index === 1) {
+      share.style.display = "flex";
+    } else {
+      copy.style.display = "flex";
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -70,6 +147,7 @@ export default class Share extends Vue {}
 }
 
 // 間距
+
 .margin-top-56 {
   margin-top: 56px;
 }
@@ -89,6 +167,11 @@ export default class Share extends Vue {}
 .margin-top-bottom-10 {
   margin-top: 10px;
   margin-bottom: 10px;
+}
+
+.padding-top-bottom-15 {
+  padding-top: 15px;
+  padding-bottom: 15px;
 }
 
 .margin-left-right-6 {
@@ -127,6 +210,10 @@ export default class Share extends Vue {}
   color: #ffffff;
 }
 
+.text-color-6a6a6a {
+  color: 6a6a6a;
+}
+
 .text-30-500 {
   font-size: 1.875rem;
   font-weight: 500;
@@ -137,9 +224,24 @@ export default class Share extends Vue {}
   font-weight: 500;
 }
 
+.text-20-300 {
+  font-size: 1.25rem;
+  font-weight: 300;
+}
+
+.text-25-500 {
+  font-size: 1.5625rem;
+  font-weight: 500;
+}
+
 .text-17-500 {
   font-size: 1.0625rem;
   font-weight: 500;
+}
+
+.text-16-300 {
+  font-size: 1rem;
+  font-weight: 300;
 }
 
 // QrCode
@@ -205,6 +307,7 @@ button {
   height: 100%;
   background-color: black;
   opacity: 0.5;
+  display: none;
 }
 
 .share {
@@ -212,17 +315,17 @@ button {
   bottom: 0px;
   left: 0px;
   width: 100%;
-  height: 21.5625rem;
+  height: auto;
   border-radius: 0.3125rem;
   background-color: #ffffff;
-  display: flex;
+  display: none;
   flex-direction: column;
 }
 
 .line {
   height: 0.0625rem;
   background-color: #f3806f;
-  width: 94%
+  width: 94%;
 }
 
 .share-row {
@@ -230,12 +333,53 @@ button {
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: center;
-
 }
 
 .share-item {
   width: 4.125rem;
- height: 6.25rem;
- background-color: red;
+  height: 6.25rem;
+}
+
+.share-app {
+  width: 4.125rem;
+  height: 4.125rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.cancel-bg {
+  width: 100%;
+  background-color: #f9f9f9;
+}
+
+.copy {
+  width: 21.25rem;
+  height: auto;
+  border-radius: 0.5rem;
+  background-color: rgba(255, 255, 255, 0.95);
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  display: none;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.flex-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.height-118 {
+  height: 7.375rem;
+  width: 100%;
+}
+
+.height-50 {
+  height: 3.125rem;
+  width: 100%;
 }
 </style>
