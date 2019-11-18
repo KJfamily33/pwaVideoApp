e1af742936b743570d56d74cffa774c2b50a83ed
Ace.c <>
2019-11-18 09:42:32 +0800

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,9 @@
 </template>
 
 <style lang="scss">
+
+@import '!/bootstrap/scss/bootstrap';
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
